9290ecdcbf29babb46888f2f8ad16db0b9e17917
Vic-Chang <s9826111@ulive.pccu.edu.tw>
2022-08-16 23:06:11 +0800

    Convert dicom to jpg

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 import requests
+import dicom2jpg
+from urllib import parse
 
 
-def download_dcm(img_url):
+def download_dcm(img_url: str):
     results = requests.get(img_url)
-
-    with open('test.dcm', 'wb') as f:
+    file_name = parse.parse_qs(parse.urlparse(img_url).query)['image'][0]
+    dcm_file = f'{file_name}.dcm'
+    with open(dcm_file, 'wb') as f:
         f.write(results.content)
+
+    # Convert to jpg
+    dicom2jpg.dicom2jpg(dcm_file)
 
 
 if __name__ == '__main__':
